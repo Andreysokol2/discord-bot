b306742c48aa20a5e8c91523a6286525125a0874
Andreysokol2 <71218421+Andreysokol2@users.noreply.github.com>
2021-02-21 12:51:12 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 settings = {
-    'token': 'Nzg2NTEwNTQyNjY3NTEzODY2.X9HdBg.hkoIxa5D2866qbXJWDc4Qec1WC0',
+    'token': 'token',
     'bot': 'Bot_valera',
     'id': 786510542667513866,
     'prefix': '!'
